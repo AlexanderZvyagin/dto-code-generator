dca10944104eba54b24ab2265ff9187690ff7e5a
Alexander Zvyagin <Zvyagin.Alexander@gmail.com>
2023-02-15 01:46:10 +0000

    Update typescript code generator.

--- cg/typescript.py
+++ cg/typescript.py
@@ -1,91 +1,113 @@
 from .all import *
 import math
 
+def typescript_type_to_string (name:str):
+
+    if name[-2:]=='[]':
+        t = typescript_type_to_string(name[:-2])
+        return f'{t}[]'
+
+    return {
+        'void'    : 'void',
+        'string'  : 'string',
+        'int'     : 'number',
+        'float'   : 'number',
+    }.get(name,name)
+
+def typescript_value_to_string (arg):
+    if type(arg)==Variable:
+        if arg.type=='type':
+            return f'new {arg.name}'
+        else:
+            return arg.name
+    elif isinstance(arg,list):
+        x = [ f'{item.name}'   for item in arg]
+        y = ','.join(x)
+        return f'[{y}]'
+    elif isinstance(arg,str):
+        return f'"{arg}"'
+    elif type(arg)==float:
+        if math.isnan(arg):
+            return 'Number.NaN'
+        else:
+            return str(arg)
+    else:
+        return str(arg)
+
+
 def File_prefix_typescript (objs):
     return [
         f'// {autogen_text}',
         ''
     ]
 
-def File_suffix_typescript (objs):
+# def File_suffix_typescript (objs):
+#     code = []
+#     code.append('export {')
+#     for i,obj in enumerate(objs):
+#         c = ',' if (i+1)<len(objs) else ''
+#         code.append(f'{indent}{obj.name}' + c)
+#     code.append('}')
+#     return code
+
+# def typescript_type (name:str):
+#     return {
+#         'void'   : 'void',
+#         'string' : 'string',
+#         'int'    : 'number',
+#         'int[]'  : 'number[]',
+#         'float'  : 'number',
+#         'float[]': 'number[]',
+#     }.get(name,name)
+
+# def typescript_value (arg):
+#     if type(arg)==float:
+#         if math.isnan(arg):
+#             return 'Number.NaN'
+#     return arg
+
+def Constructor_typescript(ctor:Function,base:Struct):
     code = []
-    code.append('export {')
-    for i,obj in enumerate(objs):
-        c = ',' if (i+1)<len(objs) else ''
-        code.append(f'{indent}{obj.name}' + c)
-    code.append('}')
+    code.append(f'constructor(')
+
+    for arg in ctor.args:
+        defval = '' if arg.defval is None else f' = {typescript_value_to_string(arg.defval)}'
+        code.append(f'{indent}{arg.name} : {typescript_type_to_string(arg.type)} {defval},')
+    code.append('){')
+
+    for name,mapping in ctor.mapping:
+        if base.base and base.base.name == name:
+            code.append(f'{indent}super(')
+            for v in mapping:
+                code.append(f'{indent*2}{typescript_value_to_string(v)},')
+            code.append(f'{indent});')
+        else:
+            assert len(mapping)==1
+            code.append(f'{indent}this.{name} = {typescript_value_to_string(mapping[0])};')
+
+    code.append(f'')
     return code
 
-def typescript_type (name:str):
-    return {
-        'void'   : 'void',
-        'string' : 'string',
-        'int'    : 'number',
-        'int[]'  : 'number[]',
-        'float'  : 'number',
-        'float[]': 'number[]',
-    }.get(name,name)
-
-def typescript_value (arg):
-    if type(arg)==float:
-        if math.isnan(arg):
-            return 'Number.NaN'
-    return arg
-
 def Function_typescript (self:Function, obj:Struct=None):
-    fname = self.name
-    ctor = False
-    derived = False
-    ftype = self.type
-    if obj:
-        derived = obj.base is not None
-        if self.name==obj.name:
-            fname = 'constructor'
-            ctor = True
-            ftype = ''
-        else:
-            ftype = f': {typescript_type(self.type)} '
 
     code = []
-    if obj:
-        code.append(f'{fname} (')
+
+    if obj and self.name==obj.name:
+        code = Constructor_typescript(self,obj)
     else:
-        code.append(f'function {fname} (')
+        ftype = typescript_type_to_string(self.type)+' '
 
-    attributes = []
-    if ctor:
-        if self.type == 'ctor-all-attributes':
-            assert len(self.args)==0
-            attributes = obj.GetAllAttributes()
-        elif self.type == 'ctor-special':
-            attributes = self.args
-        else:
-            print(f'not supported: ctor type "{self.type}"')
-    else:
-        attributes = self.args
+        code = []
+        code.append(f'{self.name} (')
 
-    args_code = []
-    for a in attributes:
-        default = '' if a.defval is None else f' = {typescript_value(a.defval)}'
-        args_code.append(f'{indent}{a.name} : {typescript_type(a.type)}{default}')
-    for i in range(len(args_code)-1):
-        args_code[i] += ','
-    code.extend(args_code)
+        for a in self.args:
+            code.append(f'{indent}')
+            default = '' if a.defval is None else f' = {typescript_value_to_string(a.defval)}'
+            code.append(f'{indent}{a.name} : {typescript_type_to_string(a.type)}{default},')
+        code.append(f') : {ftype} {{')
 
-    code.append(f') {ftype}{{')
-
-    if derived and self.type == 'ctor-all-attributes':
-        code.append(f'{indent}super (')
-        args_code = []
-        for a in obj.base.GetAllAttributes():
-            args_code.append(f'{indent*2}{a.name}')
-        for i in range(len(args_code)-1):
-            args_code[i] += ','
-        code.extend(args_code)
-        code.append(f'{indent})')
-    for line in get_lines(self.lines.get('typescript')):
+    for line in get_code(self.code.get('typescript')):
         code.append(f'{indent}{line}')
-
     code.append('}')
 
     return code
@@ -96,10 +118,10 @@ def Struct_typescript (self:Struct):
         code.append(f'class {self.name} extends {self.base.name} {{')
     else:
         code.append(f'class {self.name} {{')
-    code.append('')
+    code.append(f'')
     
     for a in self.attributes:
-        code.append(f'{indent}{a.name} : {typescript_type(a.type)};')
+        code.append(f'{indent}{a.name} : {typescript_type_to_string(a.type)};')
 
     code.append('')
 
@@ -108,9 +130,16 @@ def Struct_typescript (self:Struct):
             code.append(f'{indent}{line}')
         code.append('')
 
-    code.append(f'}}')
-    code.append('')
-    return code
+    # FIXME
+    # code.extend(Struct_compare_typescript(self))
 
-def typescript_run_test(fname):
-    asyncio.run(run(f'tsc {fname}'))
+    code.append(f'}}')
+
+    # FIXME
+    # if self.generate_json:
+    #     code.extend(Struct_to_JSON_cpp(self))
+    #     code.extend(Struct_to_JSON_string_cpp(self))
+    #     code.extend(Struct_from_JSON_cpp(self))
+    #     code.extend(Struct_from_JSON_string_cpp(self))
+
+    return code
---
+++ examples/1/output/dto.ts
@@ -0,0 +1,290 @@
+// This is an automatically generated file.
+
+class UpdaterDoc {
+
+    name : string;
+    title : string;
+    doc_md : string;
+    start : string;
+    nargs_min : number;
+    nrefs_min : number;
+
+    constructor(
+        name_ : string  = "",
+        title_ : string  = "",
+        doc_md_ : string  = "",
+        start_ : string  = "",
+        nargs_min_ : number  = -88,
+        nrefs_min_ : number  = -88,
+    ){
+        this.name = name_;
+        this.title = title_;
+        this.doc_md = doc_md_;
+        this.start = start_;
+        this.nargs_min = nargs_min_;
+        this.nrefs_min = nrefs_min_;
+    
+    }
+
+}
+
+class UpdaterDto {
+
+    name : string;
+    refs : number[];
+    args : number[];
+    start : number;
+
+    constructor(
+        name_ : string  = "",
+        refs_ : number[]  = [],
+        args_ : number[]  = [],
+        start_ : number  = Number.NaN,
+    ){
+        this.name = name_;
+        this.refs = refs_;
+        this.args = args_;
+        this.start = start_;
+    
+    }
+
+}
+
+class Updater extends UpdaterDto {
+
+    _equation : number;
+    _state : number;
+
+    constructor(
+        name : string  = "",
+        refs : number[]  = [],
+        args : number[]  = [],
+        start : number  = Number.NaN,
+    ){
+        super(
+            name,
+            refs,
+            args,
+            start,
+        );
+        this._equation = -88;
+        this._state = -88;
+    
+    }
+
+    GetStateNumber (
+    ) : number  {
+        
+        if(this._state<0)
+            throw new Error(`Updater ${this.name} has no state.`);
+        return this._state;
+        
+    }
+
+}
+
+class IndependentGaussian extends Updater {
+
+
+    constructor(
+        refs_ : number[]  = [],
+    ){
+        super(
+            "IndependentGaussian",
+            refs_,
+            [],
+            -88,
+        );
+    
+    }
+
+}
+
+class CorrelatedGaussian extends Updater {
+
+
+    constructor(
+        correlation : number  = Number.NaN,
+        state1 : number  = -88,
+        state2 : number  = -88,
+    ){
+        super(
+            "CorrelatedGaussian",
+            [state1,state2],
+            [correlation],
+            -88,
+        );
+    
+    }
+
+}
+
+class Barrier extends Updater {
+
+
+    constructor(
+        underlying : number  = -88,
+        start : number  = Number.NaN,
+        level : number  = Number.NaN,
+        direction : number  = -88,
+        action : number  = -88,
+        value : number  = Number.NaN,
+    ){
+        super(
+            "Barrier",
+            [underlying],
+            [level,value,direction,action],
+            start,
+        );
+    
+    }
+
+}
+
+class HistogramAxis {
+
+    state : number;
+    nbins : number;
+    min : number;
+    max : number;
+
+    constructor(
+        _state : number  = -88,
+        _nbins : number  = -88,
+        _min : number  = -88,
+        _max : number  = -88,
+    ){
+        this.state = _state;
+        this.nbins = _nbins;
+        this.min = _min;
+        this.max = _max;
+    
+    }
+
+}
+
+class Histogram {
+
+    x : HistogramAxis;
+    y : HistogramAxis;
+
+    constructor(
+        x : HistogramAxis  = new HistogramAxis(),
+        y : HistogramAxis  = new HistogramAxis(),
+    ){
+        this.x = x;
+        this.y = y;
+    
+    }
+
+}
+
+class EvaluationPoint {
+
+    state : number;
+    time : number;
+    value : number;
+    error : number;
+
+    constructor(
+        state_ : number  = -88,
+        time_ : number  = Number.NaN,
+        value_ : number  = Number.NaN,
+        error_ : number  = Number.NaN,
+    ){
+        this.state = state_;
+        this.time = time_;
+        this.value = value_;
+        this.error = error_;
+    
+    }
+
+}
+
+class EvaluationResults {
+
+    names : string[];
+    npaths : number[];
+    mean : number[];
+    stddev : number[];
+    skewness : number[];
+    time_points : number[];
+    time_steps : number[];
+    histograms : Histogram[];
+
+    constructor(
+        names_ : string[]  = [],
+        npaths_ : number[]  = [],
+        mean_ : number[]  = [],
+        stddev_ : number[]  = [],
+        skewness_ : number[]  = [],
+        time_points_ : number[]  = [],
+        time_steps_ : number[]  = [],
+        histograms_ : Histogram[]  = [],
+    ){
+        this.names = names_;
+        this.npaths = npaths_;
+        this.mean = mean_;
+        this.stddev = stddev_;
+        this.skewness = skewness_;
+        this.time_points = time_points_;
+        this.time_steps = time_steps_;
+        this.histograms = histograms_;
+    
+    }
+
+}
+
+class Parameter {
+
+    value : number;
+    step : number;
+    min : number;
+    max : number;
+
+    constructor(
+        value_ : number  = Number.NaN,
+        step_ : number  = Number.NaN,
+        min_ : number  = Number.NaN,
+        max_ : number  = Number.NaN,
+    ){
+        this.value = value_;
+        this.step = step_;
+        this.min = min_;
+        this.max = max_;
+    
+    }
+
+}
+
+class Model {
+
+    TimeStart : number;
+    TimeSteps : number;
+    NumPaths : number;
+    updaters : Updater[];
+    evaluations : EvaluationPoint[];
+    RunTimeoutSeconds : number;
+    MemoryLimitKB : number;
+
+    constructor(
+        TimeStart_ : number  = Number.NaN,
+        TimeSteps_ : number  = 0,
+        NumPaths_ : number  = 0,
+        updaters_ : Updater[]  = [],
+        evaluations_ : EvaluationPoint[]  = [],
+        RunTimeoutSeconds_ : number  = 1,
+        MemoryLimitKB_ : number  = 1,
+    ){
+        this.TimeStart = TimeStart_;
+        this.TimeSteps = TimeSteps_;
+        this.NumPaths = NumPaths_;
+        this.updaters = updaters_;
+        this.evaluations = evaluations_;
+        this.RunTimeoutSeconds = RunTimeoutSeconds_;
+        this.MemoryLimitKB = MemoryLimitKB_;
+    
+    }
+
+}
+
