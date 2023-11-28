9c9b2bed588cc38ee50ecfff9de4332a8e496b1e
Alexander Zvyagin <Zvyagin.Alexander@gmail.com>
2023-11-28 14:06:22 +0000

    Update.

--- examples/mcsdk/run-create-dto-and-test.sh
+++ examples/mcsdk/run-create-dto-and-test.sh
@@ -1,3 +1,3 @@
 rm -rf output
-cgdto --schema=schema.py --output=output --run-tests --languages=cpp,python,typescript
-(cp -a doc output/doc; cd doc; make html)
+cgdto --schema=schema.py --output=output --languages=cpp,python,typescript --run-tests
+(cd doc; make html)
